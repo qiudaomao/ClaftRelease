1c92babe80a7b7dff294d419a8f407959c434f2d
qiudaomao <qiudaomao@gmail.com>
2021-11-29 16:04:26 +0800

    Initial Commit

---
+++ claft/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  claft
+//
+//  Created by zfu on 2021/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ claft/claftApp.swift
@@ -0,0 +1,17 @@
+//
+//  claftApp.swift
+//  claft
+//
+//  Created by zfu on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct claftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
