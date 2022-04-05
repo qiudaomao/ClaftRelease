275e28acd7892e210071f45d041ee94b4de13bbb
qiudaomao <qiudaomao@gmail.com>
2022-04-05 17:06:09 +0800

    add dependency

---
+++ dependency/SocketRocket.xcframework/ios-arm64_x86_64-maccatalyst/SocketRocket.framework/Versions/A/Headers/SocketRocket_mac.h
@@ -0,0 +1,18 @@
+//
+//  SocketRocket_mac.h
+//  SocketRocket-mac
+//
+//  Created by zfu on 2020/11/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocketRocket_mac.
+FOUNDATION_EXPORT double SocketRocket_macVersionNumber;
+
+//! Project version string for SocketRocket_mac.
+FOUNDATION_EXPORT const unsigned char SocketRocket_macVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketRocket_mac/PublicHeader.h>
+
+
